2160839c4d699eb1d199e0dc875eda4ef14a5ab1
ayan islam <ayanislam78600@gmail.com>
2026-05-02 13:07:17 +0530

    first

---
+++ Langchain_Models/ChatModels/huggingface_models.py
@@ -0,0 +1,17 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+llm = HuggingFaceEndpoint(
+    repo_id="Qwen/Qwen2.5-72B-Instruct",
+    task="text-generation",
+    huggingfacehub_api_token=os.getenv("HUGGINGFACEHUB_ACCESS_TOKEN")
+)
+
+model = ChatHuggingFace(llm=llm)
+
+result = model.invoke("where is f1 race on coming sunday")
+
+print(result.content)
