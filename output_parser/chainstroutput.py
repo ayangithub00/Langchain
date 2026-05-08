4bf003acf1e1d1133a045b2314f98332b1171444
ayan islam <ayanislam78600@gmail.com>
2026-05-08 17:02:46 +0530

    chain output parser

---
+++ output_parser/chainstroutput.py
@@ -0,0 +1,39 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+
+llm = HuggingFaceEndpoint(
+    repo_id="Qwen/Qwen2.5-72B-Instruct",
+    task="text-generation",
+    max_new_tokens=512,
+    temperature=0.5,
+)
+
+
+model = ChatHuggingFace(llm=llm)
+
+
+# 1st prompt detailed prompt 
+template1=PromptTemplate(
+    template="Write a detailed report on {topic}",
+    input_variables=['topic']
+)
+
+# 1st prompt detailed prompt 
+template2=PromptTemplate(
+    template="Write a 5 line summary on the following text. /n {text}",
+    input_variables=['text']
+)
+
+parser = StrOutputParser()
+
+chain = template1 | model | parser | template2 | model | parser
+
+result = chain.invoke({'topic':'black hole'})
+
+print(result)
